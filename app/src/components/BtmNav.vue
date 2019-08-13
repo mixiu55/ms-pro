48336f754ab4660f3504d2438021dfe99bab47b7
yangling <carolyn30@163.com>
2019-08-13 19:23:33 +0800

    回到顶部

--- app/src/components/BtmNav.vue
+++ app/src/components/BtmNav.vue
@@ -1,9 +1,25 @@
 <template>
+<div class="bnav">
   <el-button-group>
-      <el-button type="info" icon="el-icon-chat-dot-square" title="在线消息"></el-button>
-      <el-button type="info" icon="el-icon-star-off" title="收藏"></el-button>
-      <el-button type="info" icon="el-icon-document" title="订单"></el-button>
-    </el-button-group>
+    <el-button type="info" icon="el-icon-chat-dot-square" title="在线消息"></el-button>
+    <el-button type="info" icon="el-icon-star-off" title="收藏"></el-button>
+    <el-button type="info" icon="el-icon-document" title="订单"></el-button>
+  </el-button-group>
+  <el-backtop target="">
+    <div
+      style="{
+        height: 100%;
+        width: 100%;
+        box-shadow: 0 0 6px rgba(0,0,0, .12);
+        text-align: center;
+        line-height: 40px;
+      }"
+    >
+      UP
+    </div>
+  </el-backtop>
+</div>
+  
 </template>
 <style scoped>
 .el-button-group{
@@ -11,5 +27,15 @@
   bottom: 10px;
   right: 150px;
   opacity: .8;
+}
+.bnav .el-backtop{
+  right: 105px !important;
+  bottom: 10px !important;
+  background: rgba(0,0,0,.3);
+  color: #ddd;
+  border-radius: 3px;
+}
+.bnav .el-backtop:hover{
+  color: #eee;
 }
 </style>
